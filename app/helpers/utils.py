791664baf8fdec95e649016cde07b7e280d2b7af
subhayukumar <subhayu.kumar@fiftyfivetech.io>
2025-07-28 23:16:32 +0530

    feat(ingest): add detailed historical stats module
    - Introduce `ingest_detailed_trailer_stats_data` function to ingest detailed trailer statistics
    - Add `get_stats_in_timeframe` and `get_all_stats_in_timeframe` methods in `SamsaraAPI` for fetching stats within a specified timeframe
    - Implement `backfill_samsara_detailed_stats` endpoint for backfilling data over a specified time range
    - Utilize `pandas_gbq` for efficient data transfer to BigQuery
    - Enhance parallel execution capabilities with `run_functions_in_parallel` and `run_parallel_exec_but_return_in_order`
    - Add utility functions in `pandas_utils.py` for DataFrame type analysis and column name cleaning
    - Update `ingest` module to include new ingestion functions in `__all__`
    
    This update introduces a comprehensive module for ingesting detailed historical trailer statistics, enabling efficient backfilling and parallel processing of large datasets. The changes improve data handling and integration with BigQuery, facilitating better data analysis and reporting.

--- app/helpers/utils.py
+++ app/helpers/utils.py
@@ -3,7 +3,7 @@ import json
 import logging
 import traceback
 import concurrent.futures
-from typing import Callable, Iterable
+from typing import Callable, Iterable, List, Literal, Tuple, Any
 
 def dump_json(x):
     try:
@@ -21,6 +21,22 @@ def get_trace(e: Exception, n: int = 5):
     return "".join(traceback.format_exception(e)[-n:])
 
 def run_parallel_exec(exec_func: Callable, iterable: Iterable, *func_args, **kwargs):
+    """
+    Runs a function in parallel using ThreadPoolExecutor.
+
+    Args:
+        exec_func (Callable): A function to run concurrently.
+        iterable (Iterable): An iterable to run the function on.
+        *func_args: Any additional arguments to pass to the function.
+        **kwargs: Any additional keyword arguments to pass to the function.
+
+    Returns:
+        List[Tuple[Any, Any]]: A list of tuples containing the input element and the result of the function.
+
+    Notes:
+        The `max_workers` argument can be passed as a keyword argument to set the maximum number of worker threads in the thread pool executor.
+        The `quiet` argument can be passed as a keyword argument to suppress the traceback logging for exceptions.
+    """
     func_name = (
         f"{exec_func.__name__} | parallel_exec | "
         if hasattr(exec_func, "__name__")
@@ -49,12 +65,90 @@ def run_parallel_exec(exec_func: Callable, iterable: Iterable, *func_args, **kwa
 def run_parallel_exec_but_return_in_order(
     exec_func: Callable, iterable: Iterable, *func_args, **kwargs
 ):
+    """
+    Runs a function in parallel using ThreadPoolExecutor and returns the results in the same order as the input iterable.
+
+    Args:
+        exec_func (Callable): A function to run concurrently.
+        iterable (Iterable): An iterable to run the function on.
+        *func_args: Any additional arguments to pass to the function.
+        **kwargs: Any additional keyword arguments to pass to the function.
+
+    Returns:
+        List[Any]: A list of results of the function, in the same order as the input iterable.
+
+    Notes:
+        The `max_workers` argument can be passed as a keyword argument to set the maximum number of worker threads in the thread pool executor.
+        The `quiet` argument can be passed as a keyword argument to suppress the traceback logging for exceptions.
+    """
     result = run_parallel_exec(
         exec_func, iterable := list(iterable), *func_args, **kwargs
     )
     result.sort(key=lambda x: iterable.index(x[0]))
     return [x[-1] for x in result]
 
+def run_functions_in_parallel(
+    functions: List[Callable],
+    max_workers: int = 100,
+    parallelism: Literal["thread", "process"] = "thread",
+    prefix: str = "unknown",
+    quiet: bool = False,
+):
+    """
+    Runs a list of functions in parallel using ThreadPoolExecutor.
+
+    Args:
+        functions (List[Callable]): A list of functions to run concurrently.
+        max_workers (int, optional): The maximum number of worker threads in the thread pool executor. Defaults to 100.
+        parallelism ("thread", "process"): The type of parallelism to use. Defaults to "thread".
+        prefix (str, optional): The prefix to use for the thread name. Defaults to "unknown". Only used if parallelism is "thread".
+        quiet (bool, optional): If True, suppresses the traceback logging for exceptions. Defaults to False.
+
+    Returns:
+        List[Tuple[str, Any]]: A list of tuples containing the function name and the result of the function.
+    """
+    results: List[Tuple[str, Any]] = []
+    max_workers = min(max_workers, len(functions))
+    def pool_executor():
+        if parallelism == "process":
+            return concurrent.futures.ProcessPoolExecutor(max_workers=max_workers)
+        elif parallelism == "thread":
+            return concurrent.futures.ThreadPoolExecutor(
+                max_workers=max_workers, thread_name_prefix=f"{prefix} | parallel_func | "
+            )
+    with pool_executor() as executor:
+        # Map the functions to the executor and run them in parallel
+        futures = {executor.submit(func): getattr(func, "__name__", None) for func in functions}
+        for future in concurrent.futures.as_completed(futures):
+            func_name = futures[future]
+            try:
+                results.append((func_name, future.result()))
+            except Exception as exc:
+                log_trace = exc if quiet else get_trace(exc, 3)
+                logging.error(f"Got error while running parallel_exec: {func_name}: \n{log_trace}")
+                results.append((func_name, exc))
+    return results
+
+
 def chunkify(lst: list, chunk_size: int = 40):
     for i in range(0, len(lst), chunk_size):
         yield lst[i:i + chunk_size] 
+
+def clean_name(
+    name: str,
+    replacements: dict[str, str] = {},
+    replace_dot_with_next_capital: bool = True,
+):
+    for k, v in replacements.items():
+        name = name.replace(k, v)
+
+    col_name_parts = []
+    
+    for i, x in enumerate(name.split(".")):
+        if i == 0:
+            col_name_parts.append(x)
+            continue
+        if replace_dot_with_next_capital:
+            col_name_parts.append(x[0].upper() + x[1:])
+        
+    return "".join(col_name_parts)
--- app/services/ingest.py
+++ app/services/ingest.py
@@ -2,22 +2,104 @@ import datetime
 from fastapi import APIRouter, Depends
 from fastapi.responses import JSONResponse
 
+from helpers import logger
 from logic.auth.security import verify_static_token
-from logic.ingest import ingest_ditat_data, ingest_trailer_temp_data, ingest_trailer_stats_data
+from helpers.time_utils import get_datetime_range, get_pairs_from_range, TimeUnit
+from helpers.utils import (
+    run_functions_in_parallel,
+    run_parallel_exec_but_return_in_order,
+)
+from logic.ingest import (
+    ingest_ditat_data,
+    ingest_trailer_temp_data,
+    ingest_trailer_stats_data,
+    ingest_detailed_trailer_stats_data,
+)
 
 
 router = APIRouter(prefix="/ingest", dependencies=[Depends(verify_static_token)])
 
+
 @router.post("/ditat", dependencies=[Depends(verify_static_token)])
 async def ingest_ditat():
-    result = ingest_ditat_data()
+    result = {"ditat_data": ingest_ditat_data()}
     return JSONResponse(content=result) 
+
 
 @router.post("/samsara", dependencies=[Depends(verify_static_token)])
 async def ingest_samsara_temp():
     ingested_at = datetime.datetime.now(tz=datetime.timezone.utc)
-    result = {
-        "trailer_temp": ingest_trailer_temp_data(ingested_at),
-        "trailer_stats": ingest_trailer_stats_data(ingested_at),
-    }
-    return JSONResponse(content=result) 
+    
+    def trailer_temp():
+        return ingest_trailer_temp_data(ingested_at)
+    def trailer_stats():
+        return ingest_trailer_stats_data(ingested_at)
+    def detailed_trailer_stats():
+        return ingest_detailed_trailer_stats_data(ingested_at)
+    
+    result = run_functions_in_parallel(
+        [trailer_temp, trailer_stats, detailed_trailer_stats]
+    )
+    return JSONResponse(content=dict(result))
+
+
+@router.post("/samsara/stats/backfill", dependencies=[Depends(verify_static_token)])
+async def backfill_samsara_detailed_stats(
+    start_ms: int, end_ms: int, unit: TimeUnit = TimeUnit.DAYS, granularity: int = 1
+):
+    """
+    Backfills detailed trailer stats data for a given time range in the Samsara API.
+
+    This endpoint is meant to be used for backfilling data that was missed due to any reason.
+    It takes in the start and end time in milliseconds since epoch, and will ingest the data
+    in parallel for multiple days. The `unit` parameter specifies the time granularity unit,
+    and the `granularity` parameter specifies the number of units in the time granularity.
+
+    Args:
+        start_ms (int): Start time in milliseconds since epoch.
+        end_ms (int): End time in milliseconds since epoch.
+        unit (TimeUnit): Time granularity unit. Defaults to TimeUnit.DAYS.
+        granularity (int): Number of units in the time granularity. Defaults to 1.
+
+    Returns:
+        JSONResponse: A JSON response containing the results of the backfill.
+    """
+    
+    ingested_at = datetime.datetime.now(tz=datetime.timezone.utc)
+    start_time = datetime.datetime.fromtimestamp(
+        start_ms / 1000, tz=datetime.timezone.utc
+    )
+    end_time = datetime.datetime.fromtimestamp(end_ms / 1000, tz=datetime.timezone.utc)
+    
+    # Since the data becomes huge, we run the ingestion in parallel for multiple days
+    time_range_pairs = get_pairs_from_range(
+        get_datetime_range(start_time, end_time, granularity=granularity, unit=unit)
+    )
+    logger.info(
+        f"Backfilling {len(time_range_pairs)} time ranges between "
+        f"{start_ms} and {end_ms} with granularity {granularity} {unit.value}"
+    )
+    
+    def _get_key(time_pair: tuple[datetime.datetime, datetime.datetime]):
+        start_time, end_time = time_pair
+        return f"detailed_trailer_stats_{int(start_time.timestamp()*1000)}->{int(end_time.timestamp()*1000)}"
+    
+    def _ingest(time_pair: tuple[datetime.datetime, datetime.datetime]):
+        start_time, end_time = time_pair
+        return {
+            _get_key(time_pair): ingest_detailed_trailer_stats_data(
+                start_time=start_time, 
+                end_time=end_time, 
+                ingested_at=ingested_at, 
+            )
+        }
+    
+    results = run_parallel_exec_but_return_in_order(_ingest, time_range_pairs, max_workers=8)
+    
+    result = {}
+    for time_pair, r in zip(time_range_pairs, results):
+        if not isinstance(r, dict):
+            r = { _get_key(time_pair): str(r) }
+        result.update(r)
+    
+    return JSONResponse(content=result)
