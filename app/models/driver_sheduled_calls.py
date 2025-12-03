535af81d02b22cb52aeecf9d8a6d98067811cfb3
ibrarLexumsoft <muhammad.ibrar@lexumsoft.com>
2025-12-03 22:47:13 +0500

    FETCH All records -- test

---
+++ app/models/driver_sheduled_calls.py
@@ -0,0 +1,47 @@
+from typing import Optional, List, Dict
+from sqlmodel import Field, SQLModel, Session, select
+from db import engine
+import logging
+from fastapi import HTTPException
+import httpx
+from config import settings
+import uuid
+from datetime import datetime
+
+logger = logging.getLogger(__name__)
+
+
+class DriverSheduledCalls(SQLModel, table=True):
+    __tablename__ = "call_scheduler"
+    __table_args__ = {"extend_existing": True}
+
+    id: Optional[uuid.UUID] = Field(default_factory=uuid.uuid4, primary_key=True)
+    schedule_group_id: uuid.UUID = Field(index=True)
+
+    driver: Optional[str] = None
+    reminder: Optional[str] = None
+    violation: Optional[str] = None
+
+    call_scheduled_date_time: datetime
+
+    status: bool = True
+
+    created_at: Optional[datetime] = Field(default_factory=datetime.utcnow)
+    updated_at: Optional[datetime] = Field(default_factory=datetime.utcnow)
+
+    # ---------------------------------------------------------------------
+    # DB Session
+    # ---------------------------------------------------------------------
+    @classmethod
+    def get_session(cls) -> Session:
+        return Session(engine)
+
+    # ---------------------------------------------------------------------
+    # GET ALL RECORDS
+    # ---------------------------------------------------------------------
+    @classmethod
+    def get_all_sheduled_call_records(cls) -> List["DriverSheduledCalls"]:
+        with cls.get_session() as session:
+            statement = select(cls)
+            results = session.exec(statement).all()
+            return results
--- app/services/driver_sheduled_calls_service.py
+++ app/services/driver_sheduled_calls_service.py
@@ -1,7 +1,17 @@
 from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
 from typing import List, Optional, Dict
+from models.driver_sheduled_calls import DriverSheduledCalls
 
 
 # Router with prefix + tags
 router = APIRouter(prefix="/driver_sheduled_calls", tags=["driver_sheduled_calls"])
+
+
+@router.get("/", response_model=List[DriverSheduledCalls])
+def fetch_all_driver_sheduled_calls():
+    try:
+        data = DriverSheduledCalls.get_all_sheduled_call_records()
+        return data
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
